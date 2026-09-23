e4d9b59e8454e7f52652c03cc6e30a1fa44e1fe0
skdysla <skdysla@gmail.com>
2026-09-23 16:07:36 +0900

    fix: Swagger UI 정적 리소스 접근 허용

--- gateway-server/src/main/java/com/workoutdone/rpgym/gateway/infrastructure/security/SecurityConfig.java
+++ gateway-server/src/main/java/com/workoutdone/rpgym/gateway/infrastructure/security/SecurityConfig.java
@@ -52,7 +52,8 @@ public class SecurityConfig {
                                 "/actuator/prometheus",
                                 "/swagger-ui.html",
                                 "/swagger-ui/**",
-                                "/v3/api-docs/**"
+                                "/v3/api-docs/**",
+                                "/webjars/**"
                         ).permitAll()
 
                         .pathMatchers(
